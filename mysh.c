d92a124a829b245f4f766524e02207b2f6b0fd49
Ben Cradick <ben.cradick@gmail.com>
2018-05-09 11:53:01 -0500

    started to build ps1 parser

--- mysh.c
+++ mysh.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
 #include "makeargv.h"
 
 
@@ -9,14 +10,39 @@ int main(){
     char** args;
     char *ps1 = getenv("PS1");
     //tokenized ps1
-    char** ps_tok;
-
+    char* prompt;
+    char *buf = malloc(1024);
     
-    if(getenv("PS1") == NULL){
+    
+    
+    printf("%s\n", ps1);
+    
+    if(ps1 == NULL){
          ps1 = "$";
     }
     else{
-
+        int cc;
+        for(cc = 0; cc < sizeof(ps1)-1; cc++){
+            if(*(ps1 + cc)=='\\'){
+                char current  = *(ps1 + cc + 1);
+                char *buf = malloc(sizeof(1024));
+                if(current == 'u'){
+                    strcat(prompt, getenv("USER"));
+                }
+                else if(current == 'W'){
+                    getcwd(buf, sizeof(buf));
+                    strcat(prompt, buf);
+                }
+                else if(current == 'w'){
+                    strcat(prompt, getenv("PWD"));
+                }
+                else if(current == 'h'){
+                    gethostname(buf, 1024);
+                    strtok(buf, ".");
+                        strcat(prompt, buf);
+                }
+            }
+        }
     }
     
 }
