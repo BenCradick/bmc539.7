678c4d8fba9cdcd54add949e73d947b0c78e9249
Ben Cradick <ben.cradick@gmail.com>
2018-05-10 14:35:30 -0500

    got the PS1 parse working

--- mysh.c
+++ mysh.c
@@ -8,9 +8,10 @@
 const char* myPathSet(char*);
 int main(){
     char** args;
-    char *ps1 = getenv("PS1");
+    char *ps1 = malloc(sizeof(getenv("PS1")));
+    ps1 = getenv("PS1");
     //tokenized ps1
-    char* prompt;
+    char* prompt = malloc(1024);
     char *buf = malloc(1024);
     
     
@@ -22,16 +23,23 @@ int main(){
     }
     else{
         int cc;
+        char buf[1024]; // to be used as a defualt character array buffer
+        char current; //used to house the current character value to be parsec
+
         for(cc = 0; cc < sizeof(ps1)-1; cc++){
+            printf("%c\n", *(ps1+cc));
             if(*(ps1 + cc)=='\\'){
-                char current  = *(ps1 + cc + 1);
-                char *buf = malloc(sizeof(1024));
+                current  = *(ps1 + cc + 1);
+                
                 if(current == 'u'){
                     strcat(prompt, getenv("USER"));
                 }
                 else if(current == 'W'){
                     getcwd(buf, sizeof(buf));
-                    strcat(prompt, buf);
+                    char* last = strrchr(buf, '/');
+                    strcat(prompt, last+1);
+                    printf("%s\n", buf);
+                    
                 }
                 else if(current == 'w'){
                     strcat(prompt, getenv("PWD"));
@@ -39,10 +47,20 @@ int main(){
                 else if(current == 'h'){
                     gethostname(buf, 1024);
                     strtok(buf, ".");
-                        strcat(prompt, buf);
+                    strcat(prompt, buf);
+                    
                 }
+                else if(current == 'n'){
+                    strcat(prompt, "\n");
+                }
+                cc++;
             }
+            else{
+                strncat(prompt, &ps1[cc], 1 );
+            }
+            printf("%s\n", prompt);
         }
+        
     }
     
 }
